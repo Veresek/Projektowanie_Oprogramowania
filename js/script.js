c8ff8ffab0139648bfef60f97039857afd76050f
Kacper Filipowski <coskicek@gmail.com>
2024-10-03 20:37:09 +0200

    Added multiplication and upgraded ui.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
-const btn = document.querySelector("#btn");
+const add = document.querySelector("#add");
+const mul = document.querySelector("#mul");
 let counter = document.querySelector("#counter");
-let num = 0;
-btn.addEventListener("click", () => {
-	num += 1;
+let num = Number(counter.textContent);
+function operations(operation) {
+	if (operation == "add") {
+		num += 1;
+		console.log(num);
+	} else {
+		num *= 2;
+		console.log(num);
+	}
 	counter.textContent = num;
-});
+}
+add.addEventListener("click", () => operations("add"));
+mul.addEventListener("click", () => operations("mul"));
