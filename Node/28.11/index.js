50c87c31ff45988ddb0aaf621a1f1b3892c80393
Veresek <filipowskipraca@gmail.com>
2024-11-28 15:20:51 +0100

    28.11

--- Node/28.11/index.js
+++ Node/28.11/index.js
@@ -1,1 +1,11 @@
-console.log("hello world")
+const express = require('express');
+const app = express();
+
+app.get('/:liczba1/:liczba2', (req, res) => {
+	let liczba1 = parseInt(req.params.liczba1);
+	let liczba2 = parseInt(req.params.liczba2);
+	res.send(`${liczba1 + liczba2}`);
+});
+app.listen(3000, () => {
+	console.log('Aplikacja działa');
+});
