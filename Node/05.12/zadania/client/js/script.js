60e8e5c21d03560acfc47430f0535fbb57cf7207
Kacper Filipowski <coskicek@gmail.com>
2024-12-12 01:19:05 +0100

    dawd

--- Node/05.12/zadania/client/js/script.js
+++ Node/05.12/zadania/client/js/script.js
@@ -1,12 +1,53 @@
 const btns = document.querySelectorAll('button');
 const ps = document.querySelectorAll('p');
 async function getData(url) {
-	let data = await fetch(`http://172.16.15.112:3001/${url}`);
-	data = await data.json();
+	let data = await fetch(`http://localhost:3001/${url}`);
+	data = await data.text();
 	return data;
 }
 btns[0].addEventListener('click', async () => {
 	const palindrome = document.getElementById('palindrome');
 	let res = await getData(`palindrome/${palindrome.value}`);
 	ps[0].innerHTML = res;
+});
+btns[1].addEventListener('click', async () => {
+	const ttu = document.getElementById('ttu');
+	let res = await getData(`uppercase/${ttu.value}`);
+	ps[1].innerHTML = res;
+});
+btns[2].addEventListener('click', async () => {
+	const min = document.getElementById('min');
+	const max = document.getElementById('max');
+	let res = await getData(`random/${min.value}/${max.value}`);
+	ps[2].innerHTML = res;
+});
+btns[3].addEventListener('click', async () => {
+	const reverse = document.getElementById('reverse');
+	let res = await getData(`reverse/${reverse.value}`);
+	ps[3].innerHTML = res;
+});
+btns[4].addEventListener('click', async () => {
+	const length = document.getElementById('length');
+	let res = await getData(`length/${length.value}`);
+	ps[4].innerHTML = res;
+});
+btns[5].addEventListener('click', async () => {
+	const n = document.getElementById('fibb');
+	let res = await getData(`fibonacci/${n.value}`);
+	ps[5].innerHTML = res;
+});
+btns[6].addEventListener('click', async () => {
+	const celsius = document.getElementById('celsius');
+	let res = await getData(`toFahrenheit/${celsius.value}`);
+	ps[6].innerHTML = res;
+});
+btns[7].addEventListener('click', async () => {
+	const sentance = document.getElementById('sentance');
+	let res = await getData(`reverseWords/${sentance.value}`);
+	ps[7].innerHTML = res;
+});
+btns[8].addEventListener('click', async () => {
+	const even = document.getElementById('even');
+	let res = await getData(`isEven/${even.value}`);
+	ps[8].innerHTML = res;
 });
--- Node/05.12/zadania/server/index.js
+++ Node/05.12/zadania/server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const e = require('express');
 app.use(cors());
 app.get('/palindrome/:word', (req, res) => {
 	const word = req.params.word;
@@ -11,7 +12,55 @@ app.get('/palindrome/:word', (req, res) => {
 		res.send(`Wyraz ${word} nie jest palindromem`);
 	}
 });
-
+app.get('/uppercase/:text', (req, res) => {
+	const word = req.params.text;
+	res.send(word.toUpperCase());
+});
+app.get('/random/:min/:max', (req, res) => {
+	const min = Number(req.params.min);
+	const max = Number(req.params.max);
+	res.send(`${Math.floor(Math.random() * (max - min) + min)}`);
+});
+app.get('/reverse/:text', (req, res) => {
+	const text = req.params.text;
+	let reversed = text.split('').reverse().join('');
+	res.send(reversed);
+});
+app.get('/length/:text', (req, res) => {
+	const text = req.params.text;
+	res.send(text.length + '');
+});
+app.get('/fibonacci/:n', (req, res) => {
+	let nums = [0, 1];
+	let finNums = [];
+	const n = req.params.n;
+	let fir = 0;
+	let sec = 1;
+	for (let j = 2; j < n; j++) {
+		nums.push(fir + sec);
+		if (nums.length % 2 == 1) fir = nums[nums.length - 1];
+		else sec = nums[nums.length - 1];
+	}
+	for (let i = 0; i < n; i++) {
+		finNums.push(nums[i]);
+	}
+	res.send(finNums);
+});
+app.get('/toFahrenheit/:celsius', (req, res) => {
+	const celsius = req.params.celsius;
+	let f = celsius * (9 / 5) + 32;
+	res.send(f + 'F°');
+});
+app.get('/reverseWords/:sentence', (req, res) => {
+	const sen = req.params.sentence;
+	let senRev = sen.split(' ').reverse().join(' ');
+	res.send(senRev);
+});
+app.get('/isEven/:number', (req, res) => {
+	const number = req.params.number;
+	if (number % 2 == 0) res.send(true);
+	else res.send(false);
+});
 app.listen(3001, () => {
 	console.log('wszystko ok');
 });
