46ddbd875049fb93b9e3c1f1c08d57cb87ca7b3a
Veresek <filipowskipraca@gmail.com>
2025-01-20 11:54:03 +0100

    gdfgdfg

--- Node/16.01/client/js/script.js
+++ Node/16.01/client/js/script.js
@@ -8,6 +8,7 @@ async function addBook() {
 		`http://localhost:3001/add-book/${title.value}/${author.value}`
 	);
 	sendBook = await sendBook.text();
+	alert(sendBook);
 }
 async function drawTable() {
 	let tab = document.querySelector('table');
@@ -37,12 +38,49 @@ async function drawTable() {
 		let id = document.createElement('td');
 		let title = document.createElement('td');
 		let author = document.createElement('td');
+		let edit = document.createElement('button');
+		let del = document.createElement('button');
+		edit.innerHTML = 'Edytuj';
+		del.innerHTML = 'Usuń';
 		id.innerHTML = jsn[i].id;
 		title.innerHTML = jsn[i].title;
 		author.innerHTML = jsn[i].author;
 		tr.appendChild(id);
 		tr.appendChild(title);
 		tr.appendChild(author);
+		tr.append(edit);
+		tr.append(del);
+		del.classList.add('delBtn');
+		edit.classList.add('editBtn');
+		edit.addEventListener('click', () => {
+			let id = this.previousSibling.previousSibling.previousSibling;
+			let title = this.previousSibling.previousSibling;
+			let author = this.previousSibling;
+			title.innerHTML = "<input type='text' id='title'>";
+			author.innerHTML = "<input type='text' id='author'>";
+			edit.style.display = 'none';
+			const conf = document.createElement('button');
+			conf.innerHTML = 'Zatwierdź';
+			tr.insertBefore(conf, del);
+			conf.addEventListener('click', async () => {
+				let titleVal = title.value;
+				let authorVal = author.value;
+				const upd = await fetch(
+					`http://localhost:3001/update/${id}/${titleVal}/${authorVal}`
+				);
+				upd = await upd.text();
+				alert(upd);
+				drawTable();
+			});
+		});
+		del.addEventListener('click', async () => {
+			let id =
+				this.previousSibling.previousSibling.previousSibling.previousSibling;
+			const del = await fetch(`http://localhost:3001/del/${id}`);
+			del = await del.text();
+			alert(del);
+			drawTable();
+		});
 	}
 }
 drawTable();
--- Node/16.01/server/index.js
+++ Node/16.01/server/index.js
@@ -22,13 +22,29 @@ app.get('/add-book/:title/:author', (req, res) => {
 	let author = req.params.author;
 	const sql = `insert into books (title,author) Values ('${title}','${author}')`;
 	con.query(sql, (err, result, fields) => {
-		res.send(result);
+		res.send('Pomyślnie dodano książkę');
 	});
 });
 app.get('/books', (req, res) => {
 	const sql = 'select * from books';
 	con.query(sql, (err, result, fields) => {
 		res.send(result);
+	});
+});
+app.get('/update/:id/:title/:author', (req, res) => {
+	const id = req.params.id;
+	const title = req.params.title;
+	const author = req.params.author;
+	const sql = `Update books set title = ${title}, author = ${author} where id = ${id}`;
+	con.query(sql, (err, result, fields) => {
+		res.send('Pomyślnie zmieniono dane');
+	});
+});
+app.get('/del/:id', (req, res) => {
+	const id = req.params.id;
+	const sql = `DELETE FROM books WHERE id = ${id}`;
+	con.query(sql, (err, result, fields) => {
+		res.send('Pomyślnie usunięto dane');
 	});
 });
 app.listen(3001, () => {
